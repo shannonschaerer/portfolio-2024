8fd856b3bc29a547f8ab179f0496e2b67c0da33d
Shannon Schärer <shannonschaerer@gmail.com>
2024-05-18 13:01:57 +0200

    added custom font

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,7 @@
 
 // 1. import `extendTheme` function
 import { extendTheme } from "@chakra-ui/react";
+import '@fontsource/raleway';
 
 // 2. Add your color mode config
 const config = {
@@ -9,7 +10,13 @@ const config = {
   useSystemColorMode: false,
 };
 
+
+const fonts = {
+  heading: `'Raleway', sans-serif`,
+  body: `'Raleway', sans-serif`,
+}
+
 // 3. extend the theme
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, fonts });
 
 export default theme;
